b5c2eb6002d14783ee9452ce27e20cdc9fcac196
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2025-01-26 19:29:16 +0000

    Unit-test-Martin

---
+++ app.js
@@ -0,0 +1,18 @@
+const fromDollarToYen = function (valueInDollar)  {
+    let valueInYen= valueInDollar * 146.262
+    return valueInYen
+
+
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+const fromYenToPound = function (valueInYen) {
+    let valueInPound = valueInYen * 0.0052
+    return valueInPound
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound }
---
+++ test.js
@@ -0,0 +1,25 @@
+test("One euro should be 1.07 dollars", function() {
+    const {fromEuroToDollar} =require('./app.js');
+    const dollars =fromEuroToDollar(3.5)
+    const expected = 3.5 * 1.07;
+    expect(fromEuroToDollar(3.5)).toBe(3.745)
+
+})
+test("One dollar should be 146.262 yens", function() {
+    const {fromDollarToYen} =require('./app.js');
+    const yens =fromDollarToYen(3.5)
+    const expected = 3.5 * 146.262;
+    expect(fromDollarToYen(3.5)).toBe(511.91700000000003)
+
+})
+
+test("One yen should be 0.0052 pounds", function() {
+    const {fromYenToPound} =require('./app.js');
+    const pounds =fromYenToPound(3.5)
+    const expected = 3.5 * 0.0052;
+    expect(fromYenToPound(3.5)).toBe(0.0182)
+
+})
+
+
+
