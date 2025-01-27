b2cf6b2e1645cb0ace5f172ed7696426a0d183d1
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2025-01-27 09:44:02 +0000

    unittest3

--- test.js
+++ test.js
@@ -1,23 +1,24 @@
 test("One euro should be 1.07 dollars", function() {
     const {fromEuroToDollar} =require('./app.js');
-    const dollars =fromEuroToDollar(3.5)
-    const expected = 3.5 * 1.07;
-    expect(fromEuroToDollar(3.5)).toBe(3.745)
+    const dollars =fromEuroToDollar(10)
+    const expected = 10 * 1.07;
+    expect(fromEuroToDollar(10)).toBe(10.700000000000001)
+    //????? esto no me hace sentido, deberia ser 10.7 pero no pasa la prueba
 
 })
 test("One dollar should be 146.262 yens", function() {
     const {fromDollarToYen} =require('./app.js');
-    const yens =fromDollarToYen(3.5)
-    const expected = 3.5 * 167.455;
-    expect(fromDollarToYen(3.5)).toBe(586.0925000000001)
+    const yens =fromDollarToYen(10)
+    const expected = 10 * 146.262;
+    expect(fromDollarToYen(10)).toBe(1462.62)
 
 })
 
 test("One yen should be 0.0052 pounds", function() {
     const {fromYenToPound} =require('./app.js');
-    const pounds =fromYenToPound(3.5)
-    const expected = 3.5 * 0.0052;
-    expect(fromYenToPound(3.5)).toBe(0.0182)
+    const pounds =fromYenToPound(10)
+    const expected = 10 * 0.0052;
+    expect(fromYenToPound(10)).toBe(0.052)
 
 })
 
